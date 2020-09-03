1b86472ac82be5b2953645f34ec4c7e00001c0e9
Ruslana Savuliak <savuliak9852@gmail.com>
2020-09-03 13:43:23 +0300

    Implemented method for KeepHydrated

--- src/com/ss/ita/kata/implementation/rsavu98/SixImpl.java
+++ src/com/ss/ita/kata/implementation/rsavu98/SixImpl.java
@@ -3,10 +3,49 @@ package com.ss.ita.kata.implementation.rsavu98;
 import com.ss.ita.kata.Six;
 
 public class SixImpl implements Six {
-  
+
    @Override
    public int liters (double time) {
        return (int)(time / 2);
    }
-  
+
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+        return 0;
+    }
+
+    @Override
+    public float mpgToKPM(float mpg) {
+        return 0;
+    }
+
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        return new int[0];
+    }
+
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+
+    @Override
+    public boolean amIWilson(double n) {
+        return false;
+    }
+
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        return new int[0];
+    }
 }
