fd777fcc755c1601f79fa1549bacd83559488dbb
Yuriy Dzhas <55621438+Udzhas@users.noreply.github.com>
2020-09-03 13:45:03 +0300

    initial commit

--- src/com/ss/ita/kata/implementation/udzhas/SixImpl.java
+++ src/com/ss/ita/kata/implementation/udzhas/SixImpl.java
@@ -3,5 +3,7 @@ package com.ss.ita.kata.implementation.udzhas;
 import com.ss.ita.kata.Six;
 
 public class SixImpl implements Six {
-    //....///
+    @Override
+    public int liters(double time){
+        return (int)Math.floor(time/2);
 }
