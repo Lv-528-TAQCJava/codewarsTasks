a5664ee185e1e1c23649cd0c9dd66dc889fec024
rsavu98 <60479337+rsavu98@users.noreply.github.com>
2020-09-03 11:56:45 +0300

    Update SixImpl.java

--- src/com/ss/ita/kata/implementation/rsavu98/SixImpl.java
+++ src/com/ss/ita/kata/implementation/rsavu98/SixImpl.java
@@ -3,4 +3,9 @@ package com.ss.ita.kata.implementation.rsavu98;
 import com.ss.ita.kata.Six;
 
 public class SixImpl implements Six {
+  
+   @Override
+   public int liters (double time) {
+       return (int)(time / 2);
+   }
 }
