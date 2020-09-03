ffcd1d749acc757a388fa857d7cd3b3027f913c8
Ruslana Savuliak <savuliak9852@gmail.com>
2020-09-03 18:51:48 +0300

    Implement getVolumeOfCuboid()

--- src/com/ss/ita/kata/implementation/rsavu98/SixImpl.java
+++ src/com/ss/ita/kata/implementation/rsavu98/SixImpl.java
@@ -11,7 +11,7 @@ public class SixImpl implements Six {
 
     @Override
     public double getVolumeOfCuboid(double length, double width, double height) {
-        return 0;
+        return length * width * height;
     }
 
     @Override
