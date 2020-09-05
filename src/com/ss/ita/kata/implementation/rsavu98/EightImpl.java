856526de392d82350c79ff93ea8daab147a10fa3
Ruslana Savuliak <savuliak9852@gmail.com>
2020-09-05 18:50:36 +0300

    Implemented methods 8kyu

--- src/com/ss/ita/kata/implementation/rsavu98/EightImpl.java
+++ src/com/ss/ita/kata/implementation/rsavu98/EightImpl.java
@@ -3,10 +3,10 @@ package com.ss.ita.kata.implementation.rsavu98;
 import com.ss.ita.kata.Eight;
 
 public class EightImpl implements Eight {
-  
+
     @Override
-    public int liters (double time) {
-        return (int)(time / 2);
+    public int liters(double time) {
+        return (int) (time / 2);
     }
 
     @Override
@@ -16,38 +16,72 @@ public class EightImpl implements Eight {
 
     @Override
     public float mpgToKPM(float mpg) {
-        return (float)Math.round(mpg * 1.609344 / 4.54609188 * 100) / 100;
+        return (float) Math.round(mpg * 1.609344 / 4.54609188 * 100) / 100;
     }
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+        {
+            for (int i = 0; i < array.length; i++) {
+                double sqroot = Math.sqrt(array[i]);
+                array[i] = sqroot % 1 == 0
+                        ? (int) sqroot
+                        : array[i] * array[i];
+            }
+
+            return array;
+        }
     }
 
     @Override
     public int[] countPositivesSumNegatives(int[] input) {
-        return new int[0];
+        int[] output = new int[2];
+        if (input == null || input.length == 0) {
+            return new int[0];
+        }
+        for (int value : input) {
+            if (value > 0) {
+                output[0] += 1;
+            } else output[1] += value;
+        }
+        return output;
     }
 
     @Override
     public int stringToNumber(String str) {
-        return 0;
+        return Integer.parseInt(str);
     }
 
     @Override
     public boolean amIWilson(double n) {
-        return false;
+        return n == 5 || n == 13 || n == 563;
     }
 
     @Override
     public double twoDecimalPlaces(double number) {
-        return 0;
+        return Math.round(number * 100.0) / 100.0;
     }
-  
+
 
     @Override
     public int[] divisibleBy(int[] numbers, int divider) {
-        return new int[0];
+        int arrayLength = 0;
+        for (int number : numbers) {
+            if (number % divider == 0) {
+                arrayLength++;
+            }
+        }
+        int[] output = new int[arrayLength];
+        int k = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % divider == 0) {
+                output[k] = numbers[i];
+                k++;
+            }
+        }
+
+        return output;
     }
 }
+
 
