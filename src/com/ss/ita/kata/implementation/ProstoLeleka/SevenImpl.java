21a7334607167fe782f9040a94c46aaafd8d60fb
ProstoLeleka <aliejua@gmail.com>
2020-09-06 00:31:48 +0300

    Implemented 9-13 Kata

---
+++ src/com/ss/ita/kata/implementation/ProstoLeleka/SevenImpl.java
@@ -0,0 +1,52 @@
+package com.ss.ita.kata.implementation.ProstoLeleka;
+
+import com.ss.ita.kata.Seven;
+
+import java.text.DecimalFormat;
+
+public class SevenImpl implements Seven {
+    @Override
+    public long newAvg(double[] arr, double navg){
+        double sum = 0;
+        for (double v : arr) sum += v;
+        long ans = (long) Math.ceil((arr.length + 1) * navg - sum);
+        if (ans < 0) throw new IllegalArgumentException();
+        return ans;
+    }
+
+
+    @Override
+    public String seriesSum(int n){
+
+        double sum = 1;
+        double param = 1;
+        for (int i = 1; i < n; i++) {
+        param += 3;
+        sum += 1 / param;
+        }
+        DecimalFormat df = new DecimalFormat("#.00");
+        return  String.valueOf(df.format(sum));
+
+        }
+
+    @Override
+    public int whereIsHe(int p, int bef, int aft){
+        return Math.min(p - bef, aft + 1);
+    }
+
+    @Override
+    public long findNb(long m){
+        long sum = 0;
+        long n = 0;
+        while (sum < m) {
+            n++;
+            sum += n*n*n;
+        }
+        if (sum == m) {
+            return n;
+        } else {
+            return -1;
+        }
+    }
+    }
+
