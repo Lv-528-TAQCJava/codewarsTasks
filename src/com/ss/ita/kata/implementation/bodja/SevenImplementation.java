ca0dff1915f3c754951604aea2b047dc93ee251b
Богдан Дзюбайло <banderol1024@gmail.com>
2020-09-06 18:58:18 +0300

    Removed redundant method from interface Seven.

---
+++ src/com/ss/ita/kata/implementation/bodja/SevenImplementation.java
@@ -0,0 +1,22 @@
+package com.ss.ita.kata.implementation.bodja;
+
+import com.ss.ita.kata.Seven;
+import java.util.Arrays;
+
+public class SevenImplementation implements Seven {
+
+    @Override
+    public long newAvg(double[] arr, double navg) {
+        return 0;
+    }
+
+    @Override
+    public String seriesSum(int n) {
+        return null;
+    }
+
+    @Override
+    public int whereIsHe(int p, int bef, int aft) {
+        return 0;
+    }
+}
