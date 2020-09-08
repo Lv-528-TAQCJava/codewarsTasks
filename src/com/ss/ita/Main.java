3e60be1687b7889bcacb4b92cbc1f3a078830100
Boris Yevstyhneiev <molnerats@gmail.com>
2020-09-08 14:23:52 +0300

    Creating enum Authors

--- src/com/ss/ita/Main.java
+++ src/com/ss/ita/Main.java
@@ -1,10 +1,8 @@
 package com.ss.ita;
 
+import com.ss.ita.kata.Authors;
 import com.ss.ita.kata.RunnerOld;
 
 public class Main {
-    
-    public static void main(String[] args) {
-	     System.out.println(RunnerOld.docs());
-    }
+
 }
---
+++ src/com/ss/ita/kata/Authors.java
@@ -0,0 +1,44 @@
+package com.ss.ita.kata;
+
+public enum Authors {
+    YURIY {
+        public String toString() {
+            return "Yuriy";
+        }
+    },
+    RUSLANA {
+        public String toString() {
+            return "Ruslana";
+        }
+    },
+    BORYS {
+        public String toString() {
+            return "Borys";
+        }
+    },
+    ILONA {
+        public String toString() {
+            return "Ilona";
+        }
+    },
+    DENYS {
+        public String toString() {
+            return "Denys";
+        }
+    },
+    SOLOMIA {
+        public String toString() {
+            return "Solomia";
+        }
+    },
+    BOHDAN_H {
+        public String toString() {
+            return "Bohdan H";
+        }
+    },
+    BOHDAN_D {
+        public String toString() {
+            return "Bohdan D";
+        }
+    }
+}
