4e7fbb417ec09be479ec9c54bc26f712267715ea
Udzhas <ura_dzhas@ukr.net>
2020-09-15 10:07:45 +0300

    KeepHydratedTest

---
+++ src/tests/KeepHydratedTest.java
@@ -0,0 +1,36 @@
+package tests;
+
+import static org.junit.Assert.*;
+import com.ss.ita.kata.Eight;
+import com.ss.ita.kata.implementation.udzhas.EightImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+
+@RunWith(value= Parameterized.class)
+public class KeepHydratedTest {
+
+    private final Eight Impl8 = new EightImpl();
+    private final double value;
+    private final double expected;
+
+    public KeepHydratedTest(double value, double expected) {
+        this.value=value;
+        this.expected=expected;
+    }
+
+    @Parameterized.Parameters(name= "{index}: liters({0}) == {1}")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                        {3, 1},
+                        {6.7, 3},
+                        {11.8, 5}
+                }
+        );
+    }
+    @Test
+    public void keepHydratedTest() {
+        assertEquals("Keep Hydrated Test",expected,Impl8.liters(value), 0.0001);
+    }
+}
