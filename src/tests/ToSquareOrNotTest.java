6c947dec700072e3ca1d3a175f3e0a98d85f5678
Богдан Дзюбайло <banderol1024@gmail.com>
2020-09-17 09:19:45 +0300

    Implemented 2 unit tests

--- src/tests/ToSquareOrNotTest.java
+++ src/tests/ToSquareOrNotTest.java
@@ -1,2 +1,34 @@
-package tests;public class ToSquareOrNotTest {
+package tests;
+
+import com.ss.ita.kata.Eight;
+import com.ss.ita.kata.implementation.ilona.EightImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+
+@RunWith(value = Parameterized.class)
+public class ToSquareOrNotTest {
+    private final Eight Impl8 = new EightImpl();
+    private int [] expectedArray;
+    private int [] input;
+
+    public ToSquareOrNotTest(int[] expectedArray, int[] input) {
+        this.expectedArray = expectedArray;
+        this.input = input;
+    }
+    @Parameterized.Parameters(name ="{index}:squareOrSquareRoot({0}) =={1})")
+    public static  Iterable<Object[]> data(){
+        return Arrays.asList(new Object[][]{
+                        {new int[]{4, 3, 9, 7, 2, 1}, new int[]{2, 9, 3, 49, 4, 1}}
+                }
+        );
+    }
+        @Test
+        public void squareOrSquareRoot() {
+            assertArrayEquals("Square or not to square test",expectedArray,Impl8.squareOrSquareRoot(input));
+        }
 }
--- src/tests/WhereIsHeTest.java
+++ src/tests/WhereIsHeTest.java
@@ -1,2 +1,39 @@
-package tests;public class WhereIsHeTest {
+package tests;
+
+import static org.junit.Assert.*;
+
+import com.ss.ita.kata.implementation.ilona.SevenImpl;
+import org.junit.Test;
+import com.ss.ita.kata.Seven;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+@RunWith(value = Parameterized.class)
+public class WhereIsHeTest {
+
+    private final Seven seven = new SevenImpl();
+    private int[] input;
+    private int occurrences;
+
+    public WhereIsHeTest(int[] input, int occurrences) {
+
+        this.input = input;
+        this.occurrences = occurrences;
+    }
+
+    @Parameterized.Parameters(name = "{index}:whereIsVasya({0}) =={1})")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new int[]{3, 1, 1}, 2},
+                {new int[]{5, 2, 3}, 3}}
+        );
+
+    }
+
+    @Test
+    public void whereIsHe() {
+        assertEquals("Test Where is Vasya",occurrences,seven.whereIsHe(input[0], input[1], input[2]));
+    }
 }
